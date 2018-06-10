a31859ff387026b7d5c15d6aac9e401fb5825916
gloriaJun <pureainu@gmail.com>
2018-06-10 20:49:51 +0900

    [Add] sass

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -157,14 +157,22 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.s?css$/,
             use: [
               require.resolve('style-loader'),
               {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  includePaths: [paths.styles]
+                }
               },
               {
                 loader: require.resolve('postcss-loader'),
@@ -185,7 +193,7 @@ module.exports = {
                     }),
                   ],
                 },
-              },
+              }
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
@@ -198,7 +206,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.s?css$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
