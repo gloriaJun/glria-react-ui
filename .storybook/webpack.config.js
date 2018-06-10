94c5767aa541990427933dd5b7884c93d64ffdc8
gloriaJun <pureainu@gmail.com>
2018-06-10 22:05:03 +0900

    [Add] storybook setup

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('autoprefixer')({
+                  browsers: ['last 1 version', 'ie >= 11'],
+                }),
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [path.resolve(__dirname, '../src/assets/styles', 'node_modules')],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
