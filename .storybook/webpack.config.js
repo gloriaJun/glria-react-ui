8767a779b8af80de1ec780454706bff17672c6b6
gloriaJun <pureainu@gmail.com>
2018-06-20 23:26:08 +0900

    [Modify] webpack - add eslint option

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,21 @@
 const path = require("path");
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
 
 module.exports = (storybookBaseConfig, configType) => {
+  storybookBaseConfig.module.rules.push({
+    test: /\.(js|jsx|mjs)$/,
+    enforce: 'pre',
+    use: [
+      {
+        options: {
+          formatter: eslintFormatter,
+          eslintPath: require.resolve('eslint'),
+        },
+        loader: require.resolve('eslint-loader'),
+      }
+    ]
+  });
+
   storybookBaseConfig.module.rules.push({
     test: /\.s?css$/,
     loaders: ["style-loader", "css-loader", "sass-loader"],
