080c2e05ef7d22be83e0a45ee4c166085264f3a6
gloriaJun <pureainu@gmail.com>
2018-07-03 23:43:19 +0900

    [Modify] example

--- src/example/example.jsx
+++ src/example/example.jsx
@@ -5,16 +5,22 @@ export default class Example extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
+      type: '자산',
+      category: '저축-예금',
+      name: '농협',
+      startDate: '',
+      endDate: '',
     };
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
     this.setState({
+      type: '',
+      category: '',
       name: '',
-      email: '',
+      startDate: '',
+      endDate: '',
     });
   }
 
@@ -23,24 +29,37 @@ export default class Example extends React.Component {
   }
 
   render() {
-    const {
-      name,
-      email,
-    } = this.state;
-
     return (
-      <div>
+      <div Style="padding-top: .5rem;">
         <form onSubmit={this.handleSubmit}>
           <GrTextInput
-            label="Name"
-            name="name"
-            value={name}
+            label="Category"
+            name="category"
+            value={this.state.type}
             onChange={this.handleChange}
           />
           <GrTextInput
-            label="E-mail"
-            name="email"
-            value={email}
+            label="Category"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          />
+          <GrTextInput
+            label="name"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <GrTextInput
+            label="start date"
+            name="startDate"
+            value={this.state.startDate}
+            onChange={this.handleChange}
+          />
+          <GrTextInput
+            label="end date"
+            name="endDate"
+            value={this.state.endDate}
             onChange={this.handleChange}
           />
           <button>Submit</button>
