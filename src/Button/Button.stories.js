2f3638e473c821d1ac30bd6ec297bed1261f8b68
gloriaJun <pureainu@gmail.com>
2018-07-04 00:14:41 +0900

    [Modify] button component - stories

--- src/Button/Button.stories.js
+++ src/Button/Button.stories.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
 
 import { GrButton } from '../index';
 
 storiesOf('Form:Button', module)
   .add('default', () => (
     <div>
-      <GrButton />
+      <GrButton
+        onClick={action('onClick')}
+      />
     </div>
-  ));
+  ))
+  .add('disabled', () => {
+    const disabled = boolean('disabled', true);
+
+    return (
+      <div>
+        <GrButton
+          disabled={disabled}
+        />
+      </div>
+    );
+  });
